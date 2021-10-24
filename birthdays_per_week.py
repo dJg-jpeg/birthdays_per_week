92679edf33d4fa2ef0e45998cdffea62e68ebb30
Volodymyr <vvl5656@gmail.com>
2021-10-24 15:11:20 +0300

    formatted output

--- birthdays_per_week.py
+++ birthdays_per_week.py
@@ -20,18 +20,35 @@ def parse_date_now(date_now):
     return left_date, right_date
 
 
-def get_birthdays_per_week(users):
+def get_birthdays(users):
     birthday_users = []
     l_r_date = parse_date_now(datetime.now().date())
     for birthday in users:
         day = birthday['birthday'].day
         month = birthday['birthday'].month
-        if ((datetime(year=l_r_date[0].year, month=month, day=day) > l_r_date[0])
+        if ((datetime(year=l_r_date[0].year, month=month, day=day) >= l_r_date[0])
                 and (datetime(year=l_r_date[0].year, month=month, day=day) < l_r_date[1])):
+            birthday['birthday'] = datetime(year=l_r_date[0].year, month=month, day=day).strftime('%A')
             birthday_users.append(birthday)
     return birthday_users
 
 
+def output_birthdays_next_week(users):
+    users = get_birthdays(users)
+    users_by_weekday = {}
+    for user in users:
+        try:
+            users_by_weekday[user['birthday']].append(user['name'])
+        except KeyError:
+            users_by_weekday[user['birthday']] = []
+            users_by_weekday[user['birthday']].append(user['name'])
+    for weekday, name in users_by_weekday.items():
+        print("{}: {}".format(weekday, ', '.join(name)))
+
+
 if __name__ == '__main__':
-    print(get_birthdays_per_week([{},
-                                  ]))
+    output_birthdays_next_week([{'name': 'Bill', 'birthday': datetime(year=1998, month=10, day=25)},
+                                {'name': 'Jill', 'birthday': datetime(year=1974, month=10, day=27)},
+                                {'name': 'Kim', 'birthday': datetime(year=2001, month=10, day=25)},
+                                {'name': 'Jan', 'birthday': datetime(year=1987, month=6, day=11)}
+                                ])
